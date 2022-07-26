4c2c39f0f2e6730379377a08b9daae97519d090b
Mehadi hassan <mehadihassan10712@gmail.com>
2022-07-27 00:03:08 +0600

    send data in firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 
 import './App.css';
-import {getAuth} from "firebase/auth";
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
 import app from "./firebase.init.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React, { Component }  from 'react';
+import React, { Component,useState }  from 'react';
 
 const auth = getAuth(app);
 
 function App() {
 
+const [email,setEmail ] = useState('');
+const [password,setPassword] = useState('');
+
 const handelEmail = (e) => {
 
   console.log(e.target.value);
+  setEmail(e.target.value);
 }
 const handelPass = (e) => {
 
-  console.log(e.target.value);
+  setPassword(e.target.value);
+}
+
+const handelcheck =(e) =>{
+  
 }
 
 const handelformsubmit = (e) => {
-  console.log("Submit form");
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((result) => {
+    // Signed in 
+    const user = result.user;
+    console.log (user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
   e.preventDefault();
 
 }
@@ -29,7 +48,7 @@ const handelformsubmit = (e) => {
     <div className="">
 
         <div className="reg w-50 mx-auto mt-3 mr-4">
-        <Form>
+    <Form onSubmit={handelformsubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" onBlur={handelEmail} />
@@ -41,6 +60,10 @@ const handelformsubmit = (e) => {
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" onBlur={handelPass} />
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" onBlur={handelcheck}/>
       </Form.Group>
      
       <Button variant="primary" type="submit">
