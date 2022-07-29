442d154e29904fb47b527bc8c056fb086271126d
Mehadi hassan <mehadihassan10712@gmail.com>
2022-07-29 11:15:17 +0600

    reacct route

--- src/App.js
+++ src/App.js
@@ -1,79 +1,30 @@
 
 import './App.css';
-import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
-import app from "./firebase.init.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import React, { Component,useState }  from 'react';
 
-const auth = getAuth(app);
+import React,{} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/home.js'
+import Login from './components/login/login.js'
+import Header from './components/header/header.js'
+
+
 
 function App() {
-
-const [email,setEmail ] = useState('');
-const [password,setPassword] = useState('');
-
-const handelEmail = (e) => {
-
-  console.log(e.target.value);
-  setEmail(e.target.value);
-}
-const handelPass = (e) => {
-
-  setPassword(e.target.value);
-}
-
-const handelcheck =(e) =>{
-  
-}
-
-const handelformsubmit = (e) => {
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((result) => {
-    // Signed in 
-    const user = result.user;
-    console.log (user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  e.preventDefault();
-
-}
-  return (
-    <div className="">
-
-        <div className="reg w-50 mx-auto mt-3 mr-4">
-    <Form onSubmit={handelformsubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onBlur={handelEmail} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onBlur={handelPass} />
-      </Form.Group>
-      
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" onBlur={handelcheck}/>
-      </Form.Group>
-     
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-        </div>
- 
-    </div>
+  return(
+    
+      <div className="App">
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          
+      </div>
+    
+    
   );
+
+
 }
 
 export default App;
