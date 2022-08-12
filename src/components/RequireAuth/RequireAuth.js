6fe7cf03ae4e1fd4294a4c3d74fd8ba5f85521ac
Mehadi hassan <mehadihassan10712@gmail.com>
2022-08-12 11:19:29 +0600

    ? problem solve

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,28 +1,20 @@
-import React from 'react'
-import {getAuth} from "firebase/auth"
+import React from "react";
+import { getAuth } from "firebase/auth";
 import app from "../../firebase.init";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import {
-    Navigate,
-    useLocation,
-    
-  } from "react-router-dom";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
 
 const auth = getAuth(app);
 
+const RequireAuth = ({ children }) => {
+  //children obviously in second braket
+  const [user] = useAuthState(auth);
+  let location = useLocation();
 
-
-const RequireAuth = ({children}) => { //children obviously in second braket
-    const [user] = useAuthState(auth);
-    let location = useLocation(); 
-
-    if(!user){
-        return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-
-
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
   return children;
-}
+};
 
-export default RequireAuth
+export default RequireAuth;
